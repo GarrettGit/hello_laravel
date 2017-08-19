a77b896932b33c3ecf78be1f40a3dd1a07271931
DK <lypzmnza@163.com>
2017-08-19 05:01:16 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use App\Models\User;
 
 class UsersController extends Controller
 {
-    //
-    public function create(){
-      return view('users.create');
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required'
+        ]);
+
+        //数据入库
+        $user = User::create([
+          'name'=>$request->name,
+          'email'=>$request->email,
+          'password'=>bcrypt($request->password),
+        ]);
+        //存入一条缓存的数据，让它只在下一次的请求内有效
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        
+        return redirect()->route('users.show', [$user]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,19 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+     /**
+      * [gravatar description]
+      * 使用 Gravatar 来为用户提供个人头像
+      * @param  string $size [为 gravatar 方法传递的参数 size 指定了默认值 100；]
+      * @return [type]       [description]
+      */
+    public function gravatar($size = '100'){
+      #通过 $this->attributes['email'] 获取到用户的邮箱；
+      #通过 使用 trim 方法剔除邮箱的前后空白内容；
+      #通过 用 strtolower 方法将邮箱转换为小写；
+      #通过 将小写的邮箱使用 md5 方法进行转码；
+      $hash = md5(strtolower(trim($this->attributes['email'])));
+      #将转码后的邮箱与链接、尺寸拼接成完整的 URL 并返回；
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
