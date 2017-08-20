cba48ec7769b04a496c0c237ad095c40084dfb88
DK <lypzmnza@163.com>
2017-08-20 03:33:47 +0000

    8.4

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,16 @@ use Auth;
 
 class UsersController extends Controller
 {
+  public function __construct(){
+    $this->middleware('auth',[
+      'except'=>['show','create','store']
+    ]);
+    $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+  }
+
+
     public function create()
     {
         return view('users.create');
@@ -40,5 +50,32 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
 
         return redirect()->route('users.show', [$user]);
+    }
+
+    //编辑
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    //更新
+    public function update(User $user,Request $request){
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $this->authorize('update', $user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password){
+          $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+         return redirect()->route('users.show', $user->id);
     }
 }
